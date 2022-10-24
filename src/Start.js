a18403179b4b55f5251c733b0cb31549b7b8465c
Gary <Gary.Dix@enform.nz>
2022-10-25 10:24:05 +1300

    init first save

--- src/Start.js
+++ src/Start.js
@@ -1,18 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function Start() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1>This is a heading</h1>
+        <h1>This is a StartJS</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Start;
