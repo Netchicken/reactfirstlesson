4012b3bd8e23cec2a8a05d8658274141b9634a8a
Gary <Gary.Dix@enform.nz>
2022-10-25 12:00:08 +1300

    Calculate Method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [answer, setAnswer] = useState("");
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  const Calculate = (value) => {
+    console.log("The value passed in ", value);
+
+    if (value !== "") {
+      switch (value) {
+        case "-":
+          setAnswer(first - second);
+          break;
+
+        case "+":
+          setAnswer(first + second);
+          break;
+
+        case "*":
+          setAnswer(first * second);
+          break;
+
+        case "/":
+          setAnswer(first / second);
+          break;
+        default:
+          setAnswer("Invalid");
+      }
+    }
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
