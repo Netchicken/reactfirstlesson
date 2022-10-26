a7080590c80c14ea4599c5af4e80ace2bdf1a64e
Gary <Gary.Dix@enform.nz>
2022-10-27 10:31:25 +1300

    updated and removed old functions

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ import Inputs from "./Components/Inputs";
 import ButtonKeyPad from "./Components/ButtonKeyPad";
 
 function App() {
-  //      get      set
-  const [answer, setAnswer] = useState("");
-  const [first, setFirst] = useState(4);
-  const [second, setSecond] = useState(4);
   const [calculation, setCalculation] = useState("");
-
-  useEffect(() => {
-    console.log("useEffect ", first);
-  }, [first]);
 
   const updateCalculation = (value) => {
     setCalculation(calculation + String(value));
@@ -22,33 +14,6 @@ function App() {
 
     if (value === "=") {
       setCalculation(new Function("return " + calculation));
-    }
-  };
-
-  const Calculate = (value) => {
-    // console.log("The value passed in ", value);
-
-    if (value !== "") {
-      switch (value) {
-        case "-":
-          setAnswer(first - second);
-          break;
-
-        case "+":
-          setAnswer(parseInt(first) + parseInt(second));
-          break;
-
-        case "X":
-          setAnswer(first * second);
-          break;
-
-        case "/":
-          setAnswer(first / second);
-          break;
-        default:
-          setAnswer("Invalid");
-      }
-      console.log("The value passed in ", value + " " + answer);
     }
   };
 
